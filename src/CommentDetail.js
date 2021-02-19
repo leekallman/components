7f2fb40b5b627d6100c15839def7a7c34bac506a
Lisa Lee <lisaleekallman@gmail.com>
2021-02-19 14:35:41 +0100

    Small App project

---
+++ src/CommentDetail.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+const CommentDetail = (props) => {
+
+
+    return (
+        <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={props.avatar} alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        {props.author}
+                    </a>
+                    <div className="megtadata">
+                        <span className="date">
+                        {props.date}
+                        </span>
+                    </div>
+                    <div className="text">
+                    {props.content}
+                    </div>
+                </div>
+            </div>
+    )
+}
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ApprovalCard from './ApprovalCard';
+import  CommentDetail from './CommentDetail';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail author="Joe" date="Today at 2.00PM" content="WOW" avatar="https://pngimg.com/uploads/avatar/avatar_PNG47.png"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Ken" date="Today at 9.25AM" content="YES" avatar="http://hollywoodvideo.com/wp-content/uploads/2013/08/avatar-whatshisface.jpg"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Pete" date="Yesterday at 7.06PM" content="so good!" avatar="https://cdn.pixabay.com/photo/2016/09/22/16/38/avatar-1687700__340.jpg"/>
+            </ApprovalCard>
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
